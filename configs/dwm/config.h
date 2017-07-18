db309345aba31e7a6d9ef6453bbf2921cab0cc5a
Wim Vander Schelden <lists@fixnum.org>
2017-07-18 10:01:18 +0000

    Add -u to terminator command line to run without dbus

--- configs/dwm/config.h
+++ configs/dwm/config.h
@@ -62,7 +62,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_green, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "terminator", NULL };
+static const char *termcmd[]  = { "terminator", "-u", NULL };
 static const char *webcmd[]  = { "firefox", NULL };
 
 static Key keys[] = {
