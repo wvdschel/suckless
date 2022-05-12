5c3ec7cbae8ceba9dfcb35367e3fb3ca72d7065a
Wim Vander Schelden <wim.vander-schelden@form3.tech>
2022-05-12 10:58:51 +0200

    allow overwriting X11 and Xinerama libs for dmenu

--- configs/dmenu/config.mk
+++ configs/dmenu/config.mk
@@ -5,16 +5,16 @@ VERSION = 4.6
 PREFIX = /opt/stow/suckless
 MANPREFIX = ${PREFIX}/share/man
 
-X11INC = /usr/X11R6/include
-X11LIB = /usr/X11R6/lib
+X11INC ?= /usr/X11R6/include
+X11LIB ?= /usr/X11R6/lib
 
 # Xinerama, comment if you don't want it
-XINERAMALIBS  = -lXinerama
-XINERAMAFLAGS = -DXINERAMA
+XINERAMALIBS  ?= -lXinerama
+XINERAMAFLAGS ?= -DXINERAMA
 
 # freetype
-FREETYPELIBS = -lfontconfig -lXft
-FREETYPEINC = /usr/include/freetype2
+FREETYPELIBS ?= -lfontconfig -lXft
+FREETYPEINC ?= /usr/include/freetype2
 # OpenBSD (uncomment)
 #FREETYPEINC = ${X11INC}/freetype2
 
